59ffbc3bb9122589ba3b042e244c27a84647d721
Saad <Dropheaven@live.com>
2017-05-11 14:02:41 -0400

    working on class method top_100_novels in Scraper class

--- lib/book_finder.rb
+++ lib/book_finder.rb
@@ -1,2 +1,3 @@
-require_relative "./book_finder/version"
+require_relative './book_finder/version'
 require_relative './book_finder/cli'
+require_relative './book_finder/scraper'
---
+++ lib/book_finder/scraper.rb
@@ -0,0 +1,6 @@
+class BookFinder::Scraper
+  def self.top_100_novels
+    doc = Nokogiri::HTML(open("http://www.goodreads.com/list/show/2681.Time_Magazine_s_All_Time_100_Novels"))
+    binding.pry
+  end
+end
