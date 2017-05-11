f9b50d9efc7cdb79f1d1e4ae1f48867893218bfb
Saad <Dropheaven@live.com>
2017-05-11 16:37:57 -0400

    Book.create_from_collection added.

--- lib/book_finder/book.rb
+++ lib/book_finder/book.rb
@@ -1,7 +1,15 @@
 class BookFinder::Book
   attr_accessor :title, :author, :book_link
 
+  @@all = []
+
   def intialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
+
+    @@all << self
+  end
+
+  def self.create_from_collection(book_list)
+    book_list.each {|hash| self.new(hash)}
   end
 end
