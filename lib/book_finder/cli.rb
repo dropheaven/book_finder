9b9bd5c7331b4d11cee37ab4347e42e77834bf59
Saad <Dropheaven@live.com>
2017-05-11 16:07:27 -0400

    call method displays main menu of the BookFinder gem

--- lib/book_finder/cli.rb
+++ lib/book_finder/cli.rb
@@ -3,7 +3,11 @@
 class BookFinder::CLI
 
   def call
-    puts "Top 100 Fiction and Nonfiction Books of All Time"
+    puts "Welcome To The Top 100 Fiction and Nonfiction Books of All Time"
+    puts "To List Times Magazine's Top 100 Novels => Enter 1"
+    puts "To list The Guardian's Top 100 Nonfiction Books => 2"
+    puts "To randomly generate a book => Enter 3"
+    puts "To Exit application => Enter 4"
   end
 
 end
