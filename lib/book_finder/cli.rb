91c7da34a899a1f3f97c3f9e8041d97efa32651d
Saad <Dropheaven@live.com>
2017-05-11 18:34:02 -0400

    made a box around greeting method. execute_user_input method handles first case

--- lib/book_finder/cli.rb
+++ lib/book_finder/cli.rb
@@ -3,14 +3,28 @@
 class BookFinder::CLI
 
   def call
-
+    greeting
+    execute_user_input
   end
 
   def greeting
-    puts "Welcome To The Top 100 Novels of All Time"
-    puts "To List Times Magazine's Top 100 Novels => Enter 1"
-    puts "To randomly generate a book => Enter 2"
-    puts "To Exit application => Enter 3"
+    puts "========================================================="
+    puts "|    Welcome To The Top 100 Novels of All Time          |"
+    puts "|    To List Times Magazine's Top 100 Novels => Enter 1 |"
+    puts "|    To randomly generate a book => Enter 2             |"
+    puts "|    To Exit application => Enter 3                     |"
+    puts "========================================================="
+  end
+
+  def execute_user_input
+    input = gets.strip
+
+    case input
+    when "1"
+      novels = BookFinder::Scraper.top_100_novels
+      BookFinder::Book.create_from_collection(novels)
+      BookFinder::Book.list_books
+    end
   end
 
 end
