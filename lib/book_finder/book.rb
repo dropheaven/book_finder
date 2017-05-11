b6a63f9ad4dabba4a815a190e15c31c8580a00fd
Saad <Dropheaven@live.com>
2017-05-11 19:08:20 -0400

    Scraper.random_book added. This will let the user choose a book at random.

--- lib/book_finder/book.rb
+++ lib/book_finder/book.rb
@@ -1,9 +1,13 @@
 class BookFinder::Book
-  attr_accessor :title, :author, :book_link
+  attr_accessor :title, :author, :book_link, :description
 
   @@all = []
 
-  def intialize(attributes)
+  def self.all
+    @@all
+  end
+
+  def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
 
     @@all << self
@@ -11,5 +15,16 @@ class BookFinder::Book
 
   def self.create_from_collection(book_list)
     book_list.each {|hash| self.new(hash)}
+  end
+
+  def self.list_books
+    all.each_with_index do |book, index|
+      puts "#{index + 1}. #{book.title} by #{book.author}"
+    end
+  end
+
+  def self.random_book
+    book = all.sample
+    puts "#{book.title} by #{book.author}"  
   end
 end
