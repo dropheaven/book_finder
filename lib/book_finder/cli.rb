70ce8ce516071383571dc90905722f8e273c2131
Saad <Dropheaven@live.com>
2017-05-12 14:00:23 -0400

    changed main_menu method from a case statement to a while

--- lib/book_finder/cli.rb
+++ lib/book_finder/cli.rb
@@ -4,7 +4,7 @@ class BookFinder::CLI
 
   def call
     greeting
-    main_menu_input
+    main_menu
   end
 
   def greeting
@@ -15,14 +15,15 @@ class BookFinder::CLI
     puts "    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++".blue
   end
 
-  def main_menu_input
+  def main_menu
+    novels_array = BookFinder::Scraper.top_100_novels
+    BookFinder::Book.create_from_collection(novels_array)
+    all_books = BookFinder::Book.all
+
     input = gets.strip
 
-    case input
-    when "1"
+    while input != "2"
       puts ""
-      novels = BookFinder::Scraper.top_100_novels
-      BookFinder::Book.create_from_collection(novels)
       BookFinder::Book.list_books
 
       puts ""
@@ -30,40 +31,8 @@ class BookFinder::CLI
       puts "    +   Choose a Book Number To Receive a Description     +".red
       puts "    +   Or Type Any Letter To Return To The Main Menu     +".green
       puts "    +++++++++++++++++++++++++++++++++++++++++++++++++++++++".blue
-      description_menu
 
-    when "2"
-      puts ""
-      puts "Goodbye!".bold.red
-    else
-      puts ""
-      puts "   +++++++++++++++++++++++++++++++++++++++++++++++++++++++++".blue
-      puts "   +               Invalid Entry                           +".green
-      puts "   + To List Times Magazine's Top 100 Novels => Enter 1    +".yellow
-      puts "   +         To Exit Application => Enter 2                +".red
-      puts "   +++++++++++++++++++++++++++++++++++++++++++++++++++++++++".blue
-      main_menu_input
-    end
-  end
 
-  def description_menu
-    input = gets.strip.to_i
-
-    if input.between?(1, 100)
-      book = BookFinder::Book.all[input - 1]
-      puts ""
-      puts "+++ A brief description of '#{book.title}' by #{book.author} +++".yellow
-      puts ""
-      puts BookFinder::Scraper.book_description(book.book_link)
-      puts ""
-      puts "For more information on this book, please visit:".blue
-      puts "#{book.book_link}".green
-      puts ""
-      greeting
-      main_menu_input
-    else
-      greeting
-      main_menu_input
     end
   end
 end
