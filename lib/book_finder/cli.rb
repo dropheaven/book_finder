af67326def70582659dc335ff547fcbbff4b70a0
Saad <Dropheaven@live.com>
2017-05-11 20:01:28 -0400

    added boxes around different input messages

--- lib/book_finder/cli.rb
+++ lib/book_finder/cli.rb
@@ -8,12 +8,12 @@ class BookFinder::CLI
   end
 
   def greeting
-    puts "========================================================="
-    puts "|    Welcome To The Top 100 Novels of All Time          |"
-    puts "|    To List Times Magazine's Top 100 Novels => Enter 1 |"
-    puts "|    To randomly generate a book => Enter 2             |"
-    puts "|    To Exit application => Enter 3                     |"
-    puts "========================================================="
+    puts "    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    puts "    +    Welcome To The Top 100 Novels of All Time          +"
+    puts "    +    To List Times Magazine's Top 100 Novels => Enter 1 +"
+    puts "    +    To Randomly Generate a Book => Enter 2             +"
+    puts "    +    To Exit Application => Enter 3                     +"
+    puts "    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
   end
 
   def execute_user_input
@@ -24,7 +24,35 @@ class BookFinder::CLI
       novels = BookFinder::Scraper.top_100_novels
       BookFinder::Book.create_from_collection(novels)
       BookFinder::Book.list_books
+
+      puts "    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+      puts "    +   Choose a Book Number To Receive a Description       +"
+      puts "    +    Or Type 'back' To Return To The Main Menu          +"
+      puts "    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    when "2"
+      novels = BookFinder::Scraper.top_100_novels
+      BookFinder::Book.create_from_collection(novels)
+      BookFinder::Book.random_book
+
+      puts "    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+      puts "    +         Type 'more' For a Description                 +"
+      puts "    +  Or Type 'back' To Return to the Main Menu            +"
+      puts "    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    when "3"
+
+    else
+      puts "    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+      puts "    +               Invalid Entry                           +"
+      puts "    +         Enter 1 For Complete Book List                +"
+      puts "    +         Enter 2 For a Random Selection                +"
+      puts "    +         Enter 3 to Exit Application                   +"
+      puts "    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+      execute_user_input
     end
+  end
+
+  def method_name
+
   end
 
 end
