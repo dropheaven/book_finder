7ab83ed892a078d974fe22397fb7bd23761a1e08
Saad <Dropheaven@live.com>
2017-05-12 15:03:24 -0400

    reverted back to case statement. Easier to manage inputs.

--- lib/book_finder/cli.rb
+++ lib/book_finder/cli.rb
@@ -15,24 +15,58 @@ class BookFinder::CLI
     puts "    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++".blue
   end
 
-  def main_menu
-    novels_array = BookFinder::Scraper.top_100_novels
-    BookFinder::Book.create_from_collection(novels_array)
-    all_books = BookFinder::Book.all
 
+
+  def main_menu
     input = gets.strip
 
-    while input != "2"
+    case input
+    when "1"
       puts ""
+      novels_array = BookFinder::Scraper.top_100_novels
+      BookFinder::Book.create_from_collection(novels_array)
       BookFinder::Book.list_books
+      all_books = BookFinder::Book.all
 
       puts ""
       puts "    +++++++++++++++++++++++++++++++++++++++++++++++++++++++".yellow
       puts "    +   Choose a Book Number To Receive a Description     +".red
       puts "    +   Or Type Any Letter To Return To The Main Menu     +".green
       puts "    +++++++++++++++++++++++++++++++++++++++++++++++++++++++".blue
+      description_menu
 
+    when "2"
+      puts ""
+      puts "Goodbye!".bold.red
+    else
+      puts ""
+      puts "   +++++++++++++++++++++++++++++++++++++++++++++++++++++++++".blue
+      puts "   +               Invalid Entry                           +".green
+      puts "   + To List Times Magazine's Top 100 Novels => Enter 1    +".yellow
+      puts "   +         To Exit Application => Enter 2                +".red
+      puts "   +++++++++++++++++++++++++++++++++++++++++++++++++++++++++".blue
+      main_menu
+    end
+  end
 
+  def description_menu
+    input = gets.strip.to_i
+
+    if input.between?(1, 100)
+      book = BookFinder::Book.all[input - 1]
+      puts ""
+      puts "+++ A brief description of '#{book.title}' by #{book.author} +++".yellow
+      puts ""
+      puts BookFinder::Scraper.book_description(book.book_link)
+      puts ""
+      puts "For more information on this book, please visit:".yellow
+      puts "#{book.book_link}".green
+      puts ""
+      greeting
+      main_menu
+    else
+      greeting
+      main_menu
     end
   end
 end
